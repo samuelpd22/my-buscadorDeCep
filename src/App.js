c68cd2556a34ff5c8c9305a4ebfa709ad0eacc06
samuelpd22 <samuel050101@hotmail.com>
2024-02-27 19:56:15 -0300

    Criando tela inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+     <h1 className="title">Buscar CEP</h1>
+
+     <div className="containerInput">
+      <input 
+      type="text"
+      placeholder="Digite seu cep..."
+      />
+
+      <button className="buttonSearch">
+        <FiSearch size={25} color="#000" />
+      </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 790003222</h2>
+
+        <span>Rua teste algum</span>
+        <span>Complemento : Algum complemento</span>
+        <span>Villa Rosa</span>
+        <span>Campo Grande - MS</span>
+      </main>
+     </div>
+   
   );
 }
 
