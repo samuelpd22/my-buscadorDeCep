5844a774de2d7df855501b64f2e32256918371f5
samuelpd22 <samuel050101@hotmail.com>
2024-03-05 17:50:45 -0300

    setValor

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './style.css';
 
 function App() {
+  const [input, setInput] = useState('')
   return (
     <div className="container">
      <h1 className="title">Buscar CEP</h1>
 
      <div className="containerInput">
+
       <input 
       type="text"
       placeholder="Digite seu cep..."
-      />
+       value={input} onChange={(e) => setInput(e.target.value) }/>
 
       <button className="buttonSearch">
         <FiSearch size={25} color="#000" />
